1afc2cb78c5727d4f2a2008b4fe3e10000a5b734
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-03-16 22:15:20 -0400

    DDD

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
-const ownerName = process.env.GITHUB_REPOSITORY?.split('/')[0] || ''
-const isUserSite = repoName.toLowerCase() === `${ownerName.toLowerCase()}.github.io`
-const explicitBasePath = process.env.NEXT_PUBLIC_BASE_PATH
-const basePath =
-  explicitBasePath !== undefined
-    ? explicitBasePath
-    : process.env.GITHUB_ACTIONS && repoName && !isUserSite
-      ? `/${repoName}`
-      : ''
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
